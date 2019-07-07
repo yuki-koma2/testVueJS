91b892bc0f719bf661dc8477d83b4e760fce31bc
yuki komatsu <yuki.k.lion@gmail.com>
2019-07-07 10:24:29 +0900

    little change

--- testvue.js
+++ testvue.js
@@ -1,73 +1,42 @@
-// window.onload = function(){
-//   console.log("onload");
-// };
 
-//test
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-});
-app.message = 'I have change the messsage data!';
-
-var app2 = new Vue({
-  el: '#app2',
-  data: {
-    seen: true
-  }
-});
-app2.seen = false;
-
-var app3 = new Vue({
-  el: '#app3',
-  data: {
-    todos:[
-      {text: 'Lean Vue'},
-      {text: 'second'},
-      {text: 'List type'}
-    ]
-  }
-});
-app3.todos.push({text:'New text'});
-
-var app4 = new Vue({
-  el: '#app4',
+var  app2 = new Vue({
+  el:'#app2',
   data:{
-    message: 'Hello Vue!'
-  },
-  methods:{
-    reverseMessage: function() {
-      this.message = this.message.split('').reverse().join('')
-    }
+    message:'you loaded this page on' + new Date().toLocaleString()
   }
-});
-
-var app5 = new Vue({
-  el: '#app5',
-  data:{
-    message: 'model input'
-  }
-});
-
-Vue.component('todo-item', {
-  template: '<li>This is a todo</li>'
 })
 
 
-Vue.component('todo-item-2', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-});
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
 
-var app6 = new Vue({
-    el: '#app6',
-    data: {
-      groceryList: [
-        { id: 0, text: 'Vegetables' },
-        { id: 1, text: 'Cheese' },
-        { id: 2, text: 'Whatever else humans are supposed to eat' }
-      ]
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ],
+    firstName:'Foo',
+    lastName:'Bar'
+  },
+  computed:{
+    fullName: {
+      //getter
+      get: function () {
+        return this.firstName + ' ' + this.lastName
+      },
+      //setter
+      set: function (newName) {
+        const names = newName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
     }
-});
-//end of test
+  }
+})
+
+
